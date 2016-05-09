11c08386c0d631fd3906efec90690c0bf6d6f284
hirekatsu <izumibobcat1@gmail.com>
2016-05-09 12:10:47 +0900

    my dqa 1

---
+++ mydqa1/my_settings.py
@@ -0,0 +1,36 @@
+{
+    'server': {
+        'host': 's2.ac5g.ac.symantec.com',
+        'api_key': 'pw/zWuo=',
+        'packbund': 'com.appcenterhq.s2.installer',
+        'workhub': 'ac5g s2',
+        'username': 'ki',
+        'password': 'password',
+        'email_password': 'symantec2014'
+    },
+    'local': {
+        'out_folder': 'H:\\_MINE\\_AC\\151002_test541_5\\',
+        'log_file_name': 'TAoneJ1.log',
+        'debug_file_name': 'TAoneJ1.debug.log',
+        'UiA_JAR_name': 'TAoneJ1.jar',
+        'UiA_JAR_folder': 'D:\\DATA\\BoxSync\\Box Sync\\MyBox\\SMS\\TAone\\uiautomator\\TAoneJ1\\bin\\',
+        # 'UiA_JAR_folder': '~/Desktop/DATA/Box Sync/Box Sync/MyBox/SMS/TAone/uiautomator/TAoneJ1/bin/',
+        'UiA_packbund': 'com.symantec.jdc.mobility.uiautomator',
+        'apk_folder': './apk/',
+        'apk_files': {
+            'com.symantec.mobile.securebrowser': 'com.symantec.mobile.securebrowser.20150522.apk',
+            'com.symantec.securemail': 'com.symantec.securemail.20150726.apk',
+            'com.symantec.jdc.taoneapp': 'com.symantec.jdc.taoneapp.base.apk',
+            'com.symantec.jdc.taonebuddy': 'com.symantec.jdc.taonebuddy.base.apk',
+            'com.symantec.jdc.taoneutil': 'com.symantec.jdc.taoneutil.base.apk',
+            'com.activator.workmail': 'com.activator.workmail.apk'
+        },
+        'testweb_url': 'http://testweb1.ac.symantec.com'
+    },
+    'command': {
+        'adb': 'D:\\Programs\\Android\\sdk\\platform-tools\\adb.exe',
+        'curl': '.\\bin\\curl.exe'
+        # 'adb': 'D:\\Programs\\Android\\sdk\\platform-tools\\adb.exe',
+        # 'curl': '.\\bin\\curl.exe'
+    }
+}
